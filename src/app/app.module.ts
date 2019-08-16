efe95bec843406253e3b64afe849b7fe84fc96d8
Andrey Sverkunov <andrey.sverkunov@citi.com>
2019-08-16 18:18:07 +0300

    add error and success notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { InviteListComponent } from './invite-list/invite-list.component';
 import { InviteComponent } from './invite/invite.component';
@@ -17,6 +19,11 @@ import { InviteComponent } from './invite/invite.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      disableTimeOut: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
