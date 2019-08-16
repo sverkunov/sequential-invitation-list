847e60d70331622ee8856419b322cf315759aaab
Andrey Sverkunov <andrey.sverkunov@citi.com>
2019-08-16 14:16:44 +0300

    initial commit

---
+++ src/app/invite/invite.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../service/invite.service';
+
+const users: User[] = [
+  { email: 'user0@comtravo.com' },
+  { email: 'user1@comtravo.com' },
+  { email: 'user2@comtravo.com' },
+  { email: 'user3@comtravo.com' },
+  { email: 'user4@comtravo.com' },
+  { email: 'user5@comtravo.com' },
+  { email: 'user6@comtravo.com' },
+  { email: 'user7@comtravo.com' },
+  { email: 'user8@comtravo.com' },
+  { email: 'user9@comtravo.com' },
+  { email: 'user10@comtravo.com' }
+];
+
+@Component({
+  selector: 'app-invite',
+  templateUrl: './invite.component.html',
+  styleUrls: ['./invite.component.css']
+})
+export class InviteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    alert('Invite users');
+  }
+}
